97954d9ab38ef581e7b54f384e422470778c058b
Andrey Paschenko <33546220+paschenkoas@users.noreply.github.com>
2018-11-21 16:53:05 +0200

    Update CountUnit.java

--- CountUnit.java
+++ CountUnit.java
@@ -14,19 +14,8 @@ public class CountUnit {
         }
         System.out.println("введенное число - "+n+" в двоичной системе - "+number_2);
         System.out.println(count1+" единицы в двоичной системе счисления в числе "+n);
-
         binar(n);
-
-       /* String number_3 = binar(n);
-       int count2=0;
-        char[] strToArray1 =number_3.toCharArray();
-        for (int i = 0; i < strToArray1.length; i++) {
-            if (strToArray1[i]=='1')count2++;
-        }
-        System.out.println(count2+" единицы в двоичной системе счисления в числе "+n);
-    */
     }
-
     public static void binar(int a){  //перевод числа в двоичную систему счисления без использования Integer.toBinaryString
         int b;
         String temp = "";
@@ -37,5 +26,4 @@ public class CountUnit {
         }
         System.out.print(temp);
     }
-
 }
